87bc797e2cee6d697cb60050349cc71f205a9f0d
Bevilhonda <marcelinyuri@gmail.com>
2026-05-28 19:50:28 -0300

    atualiza controler barbeiro

--- src/main/java/com/barbearia/api/controller/BarbeiroController.java
+++ src/main/java/com/barbearia/api/controller/BarbeiroController.java
@@ -1,4 +1,53 @@
 package com.barbearia.api.controller;
 
+import com.barbearia.api.entity.Barbeiro;
+import com.barbearia.api.service.BarbeiroService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/barbeiros")
 public class BarbeiroController {
+
+    private final BarbeiroService barbeiroService;
+
+    public BarbeiroController(BarbeiroService barbeiroService) {
+        this.barbeiroService = barbeiroService;
+    }
+
+    @PostMapping
+    public Barbeiro salvar(@RequestBody Barbeiro barbeiro) {
+        return barbeiroService.salvarBarbeiro(barbeiro);
+    }
+
+    @GetMapping
+    public List<Barbeiro> listar() {
+        return barbeiroService.listarBarbeiros();
+    }
+
+    @GetMapping("/{id}")
+    public Barbeiro buscarPorId(@PathVariable Long id) {
+        return barbeiroService.buscarPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public Barbeiro atualizar(
+            @PathVariable Long id,
+            @RequestBody Barbeiro barbeiro
+    ) {
+        return barbeiroService.atualizarBarbeiro(id, barbeiro);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deletar(@PathVariable Long id) {
+
+        boolean deletado = barbeiroService.deletarBarbeiro(id);
+
+        if (!deletado) {
+            return "Barbeiro não encontrado";
+        }
+
+        return "Barbeiro deletado com sucesso";
+    }
 }
