e0589261c9ec5b729601b1f2ac0195d06f363c7f
Bevilhonda <marcelinyuri@gmail.com>
2026-05-29 08:14:04 -0300

    cria agendamento service

---
+++ src/main/java/com/barbearia/api/service/AgendamentoService.java
@@ -0,0 +1,63 @@
+package com.barbearia.api.service;
+
+import com.barbearia.api.entity.Agendamento;
+import com.barbearia.api.entity.Barbeiro;
+import com.barbearia.api.entity.Cliente;
+import com.barbearia.api.repository.AgendamentoRepository;
+import com.barbearia.api.repository.BarbeiroRepository;
+import com.barbearia.api.repository.ClienteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AgendamentoService {
+
+    private final AgendamentoRepository agendamentoRepository;
+    private final ClienteRepository clienteRepository;
+    private final BarbeiroRepository barbeiroRepository;
+
+    public AgendamentoService(AgendamentoRepository agendamentoRepository,
+                              ClienteRepository clienteRepository,
+                              BarbeiroRepository barbeiroRepository) {
+        this.agendamentoRepository = agendamentoRepository;
+        this.clienteRepository = clienteRepository;
+        this.barbeiroRepository = barbeiroRepository;
+    }
+
+    public Agendamento salvar(Long clienteId,
+                              Long barbeiroId,
+                              Agendamento agendamento) {
+
+        Cliente cliente = clienteRepository.findById(clienteId)
+                .orElseThrow(() ->
+                        new RuntimeException("Cliente não encontrado"));
+
+        Barbeiro barbeiro = barbeiroRepository.findById(barbeiroId)
+                .orElseThrow(() ->
+                        new RuntimeException("Barbeiro não encontrado"));
+
+        agendamento.setCliente(cliente);
+        agendamento.setBarbeiro(barbeiro);
+
+        return agendamentoRepository.save(agendamento);
+    }
+
+    public List<Agendamento> listar() {
+        return agendamentoRepository.findAll();
+    }
+
+    public Agendamento buscarPorId(Long id) {
+        return agendamentoRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Agendamento não encontrado"));
+    }
+
+    public void deletar(Long id) {
+        Agendamento agendamento = agendamentoRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Agendamento não encontrado"));
+
+        agendamentoRepository.delete(agendamento);
+    }
+}
