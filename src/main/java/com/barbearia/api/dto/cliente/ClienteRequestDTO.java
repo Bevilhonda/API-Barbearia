4f9c499c8593fee183d4e0b144593468b89556ec
Bevilhonda <marcelinyuri@gmail.com>
2026-05-29 17:32:30 -0300

    criação de DTO para cliente, requestdto e responsedto , entrada e saida dto

--- src/main/java/com/barbearia/api/dto/cliente/ClienteRequestDTO.java
+++ src/main/java/com/barbearia/api/dto/cliente/ClienteRequestDTO.java
@@ -1,4 +1,36 @@
 package com.barbearia.api.dto.cliente;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class ClienteRequestDTO {
+
+    @NotBlank(message = "Nome não pode ser vazio")
+    private String nome;
+
+    @NotBlank(message = "Telefone não pode ser vazio")
+    private String telefone;
+
+    public ClienteRequestDTO() {
+    }
+
+    public ClienteRequestDTO(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
