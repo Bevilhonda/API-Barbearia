ce4130c94ebf26a113736b1ed012593a55a28248
Bevilhonda <marcelinyuri@gmail.com>
2026-05-29 08:23:14 -0300

    inclui função que faz automaticamente uma query de agendamento

--- src/main/java/com/barbearia/api/repository/AgendamentoRepository.java
+++ src/main/java/com/barbearia/api/repository/AgendamentoRepository.java
@@ -3,7 +3,24 @@ package com.barbearia.api.repository;
 import com.barbearia.api.entity.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Optional;
 
 @Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+
+    Optional<Agendamento> findByBarbeiroIdAndDataAgendamentoAndHorario(
+            Long barbeiroId,
+            LocalDate dataAgendamento,
+            LocalTime horario
+    );
+    /* com essa função o Spring vai automaticamente gerar:
+    SELECT *
+    FROM agendamento
+    WHERE barbeiro_id = ?
+    AND data_agendamento = ?
+    AND horario = ?
+    */
+
 }
