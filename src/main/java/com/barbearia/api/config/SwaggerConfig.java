1b8ffab41b409b7bfab38d98a1490d58d0aea5dd
Bevilhonda <marcelinyuri@gmail.com>
2026-06-01 19:27:13 -0300

    inclui imports e script para  swagger

--- src/main/java/com/barbearia/api/config/SwaggerConfig.java
+++ src/main/java/com/barbearia/api/config/SwaggerConfig.java
@@ -1,4 +1,26 @@
 package com.barbearia.api.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Barbearia")
+                        .version("1.0")
+                        .description("Sistema de agendamento para barbearia")
+                        .contact(new Contact()
+                                .name("Marcelo Bevilacqua de Andrade")
+                        )
+                );
+    }
 }
